9abd3638698ab041049e66d82a406c3bcd13b7e5
Alexander Toloza <tolozakevin96@gmail.com>
2021-07-16 15:25:14 -0600

    fUNCION DE INSERTAR

--- main.py
+++ main.py
@@ -5,47 +5,63 @@ import parametros as db
 import query as q
 from datetime import datetime
 import mysql.connector
-from mysql.connector import Error
+import time
+import pytz
 
+def local_time():
+    tzone = pytz.timezone('America/El_Salvador')
+    tz_dtime = tzone.localize(datetime.now())
+    return tz_dtime
 
 def Consultar_as400(query):
     try:
-        connection = pyodbc.connect(driver=db.driver, system=db.system, uid=db.uid, pwd=db.pwd)
-        c1 = connection.cursor()
-        c1.execute(query)
-        row = c1.fetchall()
-        valores = []
-        for rows in row:
-            valores.append(rows)
-        insertar_mysql(valores)
-        print("Se inserto:", valores)
-        c1.close()
-        connection.close()
+        conexion_as400 = pyodbc.connect(driver=db.driver, system=db.system, uid=db.uid, pwd=db.pwd)
+        cursor = conexion_as400.cursor()
+        cursor.execute(query)
+        filas_obtenidas = cursor.fetchall()
+        cursor.close()
+        conexion_as400.close()
     except pyodbc.Error as err:
         print(datetime.now())
         print("Error as400: ", err)
         print("Query: ", query)
         return None
     else:
-        return None
+        return tuple(filas_obtenidas)
 
 def insertar_mysql(valores):
     try:
-        conn = mysql.connector.connect(**db.mysql_args)
-        if conn.is_connected():
-            cur = conn.cursor()
-            cur.execute(q.insert_horario_escenario1, valores)
-            cur.close()
-            conn.close()
+        conexion_mysql = mysql.connector.connect(**db.mysql_args)
+        if conexion_mysql.is_connected():
+            cursor = conexion_mysql.cursor()
+            cursor.executemany(q.insert_horario_escenario1, valores)
+            cursor.close()
+            conexion_mysql.close()
     except mysql.connector.Error as err:
         print(datetime.now())
         print("Error mysql: ", err)
+
         return None
     else:
         return None
 
+
+def constructor_insert(datos):
+    valores = []
+    try:
+        for val in datos:
+            valores.append(val)
+        return valores
+    except Exception as error:
+        print("Error sentencia sql;", error)
+
+
 def main():
-    Consultar_as400(q.query_horario)
+
+    valores = Consultar_as400(q.query_escenario1)
+    recibido = constructor_insert(valores)
+    insertar_mysql(recibido)
 
 if __name__ == '__main__':
     main()
+
